351cced66f89d8a03499eef63401800d97d54389
Viktoriia <pecourses>
2023-04-02 19:25:25 +0300

    Add model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+const { GENDERS } = require('../constants');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -9,21 +8,46 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate (models) {
       // define association here
     }
   }
-  User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    passwordHash: DataTypes.STRING,
-    birthday: DataTypes.DATEONLY,
-    gender: DataTypes.STRING,
-    image: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  User.init(
+    {
+      firstName: {
+        type: DataTypes.STRING(64),
+        allowNull: false,
+        validate: { is: /^[A-Z][a-z]+$/, len: [2, 64] },
+      },
+      lastName: {
+        type: DataTypes.STRING(64),
+        allowNull: false,
+        validate: { is: /^[A-Z][a-z]+$/, len: [2, 64] },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: { isEmail: true },
+      },
+      passwordHash: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      birthday: {
+        type: DataTypes.DATEONLY,
+        validate: { isDate: true, isBefore: new Date().toISOString() },
+      },
+      gender: {
+        type: DataTypes.STRING,
+        validate: { isIn: [GENDERS] },
+      },
+      image: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    }
+  );
   return User;
 };
