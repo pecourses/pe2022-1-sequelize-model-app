997b71b04f1d388564e13c6914143b65cd33bd91
Viktoriia <pecourses>
2023-04-02 17:56:35 +0300

    Add app

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api', router);
+
+app.use((err, req, res, next) => {
+  console.log('err :>> ', err);
+  res.status(500).send('Server Error');
+});
+
+module.exports = app;
+
+// GET /api/users
+// POST /api/users
+// GET /api/users/1
+// UPDATE /api/users/1
+// DELETE /api/users/1
