1a2003720ef43c4efed7f260fff159c3a6636ab6
Viktoriia <pecourses>
2023-04-08 15:56:37 +0300

    Add post request

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,10 +1,25 @@
 const { Router } = require('express');
 const usersRouter = Router();
+const { User } = require('./../models');
+
+// POST /api/users body
 
 usersRouter
   .route('/')
-  .get(() => {})
-  .post(() => {});
+  .post(async (req, res, next) => {
+    // дістати дані з body
+    const { body } = req;
+    try {
+      // спробувати створити нового користувача в БД
+      const createdUser = await User.create(body);
+      // ок - відправити 200 + створеного користувача
+      console.log('body :>> ', body);
+      console.log('createdUser :>> ', createdUser);
+      res.status(200).send();
+    } catch (e) {}
+    // не ок - відправити 4** або 5** + помилку
+  })
+  .get(() => {});
 usersRouter
   .route('/userId')
   .get(() => {})
