1a09262f444e8ea01e988b29d4b4849f0a9394b3
Viktoriia <pecourses>
2023-04-08 16:17:20 +0300

    Add post controllers + requests

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const usersRouter = Router();
+const _ = require('lodash');
 const { User } = require('./../models');
 
 // POST /api/users body
@@ -13,11 +14,16 @@ usersRouter
       // спробувати створити нового користувача в БД
       const createdUser = await User.create(body);
       // ок - відправити 200 + створеного користувача
-      console.log('body :>> ', body);
-      console.log('createdUser :>> ', createdUser);
-      res.status(200).send();
-    } catch (e) {}
-    // не ок - відправити 4** або 5** + помилку
+      const preparedUser = _.omit(createdUser.get(), [
+        'passwordHash',
+        'createdAt',
+        'updatedAt',
+      ]);
+      res.status(200).send(preparedUser);
+    } catch (e) {
+      // не ок - відправити 4** або 5** + помилку
+      next(e);
+    }
   })
   .get(() => {});
 usersRouter
