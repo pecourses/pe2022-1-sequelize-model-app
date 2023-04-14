a169266c6d548dcfe7010ba7c8034efc13032485
Viktoriia <pecourses>
2023-04-14 14:28:50 +0300

    Refactor enpoint /api/tasks handling

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,11 @@
 const { Router } = require('express');
 const usersRouter = require('./usersRouter');
-const { Task, User } = require('./../models');
+const tasksRouter = require('./tasksRouter');
 
 const router = Router();
 
 router.use('/users', usersRouter);
 
-const tasksRouter = Router();
-
 router.use('/tasks', tasksRouter);
-
-tasksRouter.get('/', async (req, res, next) => {
-  try {
-    const foundTasks = await Task.findAll({
-      raw: true,
-      attributes: { exclude: ['createdAt', 'updatedAt'] },
-      include: {
-        model: User,
-        attributes: ['firstName', 'lastName'],
-      },
-    });
-
-    res.status(200).send({ data: foundTasks });
-  } catch (e) {
-    next(e);
-  }
-});
 
 module.exports = router;
