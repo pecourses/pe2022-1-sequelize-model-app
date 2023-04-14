d13f1d9acf3ecf7d4095a871262e6cf9c0f93113
Viktoriia <pecourses>
2023-04-14 15:01:53 +0300

    Add Sequelize error handler

--- middleware/errorHandlers.js
+++ middleware/errorHandlers.js
@@ -1,3 +1,28 @@
+const createError = require('http-errors');
+const {
+  Sequelize: { BaseError, ValidationError: SequelizeValidationError },
+} = require('./../models');
+
+module.exports.dbErrorHandler = (err, req, res, next) => {
+  // Відловлюємо помилки Sequelize (https://sequelize.org/api/v6/identifiers.html#errors)
+
+  // Помилки валідації (невалідне ім'я, неунікальне мило тощо)
+  if (err instanceof SequelizeValidationError) {
+    // згідно з json:api запаковуємо одразу корисну інфу з об'єкту помилки
+    // одразу в масив об'єктів з полями status, message
+    const errors = err.errors.map(e => ({ status: 422, title: e.message }));
+
+    return res.status(422).send({ errors });
+  }
+
+  // Інші помилки Sequelize
+  if (err instanceof BaseError) {
+    next(createError(500, 'Database Error'));
+  }
+
+  next(err);
+};
+
 module.exports.errorHandler = (err, req, res, next) => {
   if (res.headersSent) {
     return;
