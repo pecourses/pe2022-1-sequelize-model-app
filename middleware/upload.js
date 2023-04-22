30bcad60d5725a0c7700b0e796c068c8cdee809e
Viktoriia <pecourses>
2023-04-22 18:10:06 +0300

    Add parameters to multer

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,6 +1,23 @@
 const multer = require('multer');
+const path = require('path');
 const { STATIC_PATH } = require('../constants');
 
-const upload = multer({ dest: STATIC_PATH });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(STATIC_PATH, 'images'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now());
+  },
+});
+
+// gif, jpeg, png
+function fileFilter (req, file, cb) {
+  // if (file.mimetype === 'image/gif' ||file.mimetype === 'image/jpeg' ||file.mimetype === 'image/png')
+  const MIMETYPE_REGEXP = /^image\/(gif|jpeg|png)$/;
+  cb(null, MIMETYPE_REGEXP.test(file.mimetype));
+}
+
+const upload = multer({ storage, fileFilter });
 
 module.exports.uploadUserPhoto = upload.single('userPhoto');
