edb2607f7fa4b56bf0a60218e589fdab19013296
Viktoriia <pecourses>
2023-04-22 16:08:04 +0300

    Add .sequelizerc

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const PORT = process.env.PORT || 5000;
 
 const httpServer = http.createServer(app);
 
-console.log('process.env.PORT :>> ', process.env);
-
 httpServer.listen(PORT, () =>
   console.log(`Server is listening http://localhost:${PORT}`)
 );
